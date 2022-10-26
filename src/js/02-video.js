f45a4f39c0948ce4a0ee5ed02a3881b56ea844d9
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-10-26 15:36:23 +0300

    task-2 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(playTime, 1000));
+
+function playTime({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+const storage = localStorage.getItem('videoplayer-current-time');
+
+if (storage) {
+  player.setCurrentTime(storage);
+}
