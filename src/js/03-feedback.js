0090359b56faf4202a0a215f5e4fd023dc9e2ef3
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-10-28 12:28:35 +0300

    task-3 complete

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+onFillingForm();
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener(
+  'input',
+  throttle(e => {
+    let completedForm = localStorage.getItem(STORAGE_KEY);
+    completedForm = completedForm ? JSON.parse(completedForm) : {};
+    completedForm[e.target.name] = e.target.value;
+    console.log(completedForm);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(completedForm));
+  }),
+  500
+);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  console.log(localStorage.getItem(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFillingForm() {
+  const savedStorage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedStorage) {
+    const savedForm = JSON.parse(savedStorage);
+    console.log(savedForm);
+    Object.entries(savedForm).forEach(([name, value]) => {
+      formEl.elements[name].value = value;
+    });
+  }
+}
