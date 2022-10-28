2d0e48c6cf2639b512e3a6859cf0e4269a07e4a8
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-10-28 12:35:56 +0300

    task-3 edits

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,3 @@
-// import trottle from 'lodash.throttle';
-
-// const form = document.querySelector('form');
-// const STORAGE_KEY = 'feedback-form-state';
-// //const formData = {};
-// populateForm();
-
-// form.addEventListener('submit', onSubmitedForm)
-// form.addEventListener('input', trottle(
-// e =>{
-//     let completedForm = localStorage.getItem(STORAGE_KEY);
-//     completedForm = completedForm ? JSON.parse(completedForm) : {};
-//     completedForm[e.target.name] = e.target.value
-//     console.log(completedForm)
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(completedForm))
-// }), 500)
-
-// function onSubmitedForm (e){
-//     e.preventDefault();
-//     e.currentTarget.reset();
-//     console.log(localStorage.getItem(STORAGE_KEY));
-//     localStorage.removeItem(STORAGE_KEY);
-//     }
-
-// function populateForm(){
-//     const savedStorage = localStorage.getItem(STORAGE_KEY)
-
-//     if(savedStorage){
-//         const savedForm = JSON.parse(savedStorage);
-//         console.log(savedForm)
-//         Object.entries(savedForm).forEach(([name, value]) => {
-//             form.elements[name].value = value; })
-
-//     }
-// }
-
 import trottle from 'lodash.throttle';
 
 const form = document.querySelector('form');
