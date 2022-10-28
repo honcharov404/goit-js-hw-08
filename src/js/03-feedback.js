32e43fb36238c180672f7a41f66191093a198552
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-10-28 12:34:27 +0300

    task-3 edits

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,50 @@
-import throttle from 'lodash.throttle';
+// import trottle from 'lodash.throttle';
 
-const formEl = document.querySelector('.feedback-form');
+// const form = document.querySelector('form');
+// const STORAGE_KEY = 'feedback-form-state';
+// //const formData = {};
+// populateForm();
+
+// form.addEventListener('submit', onSubmitedForm)
+// form.addEventListener('input', trottle(
+// e =>{
+//     let completedForm = localStorage.getItem(STORAGE_KEY);
+//     completedForm = completedForm ? JSON.parse(completedForm) : {};
+//     completedForm[e.target.name] = e.target.value
+//     console.log(completedForm)
+//     localStorage.setItem(STORAGE_KEY, JSON.stringify(completedForm))
+// }), 500)
+
+// function onSubmitedForm (e){
+//     e.preventDefault();
+//     e.currentTarget.reset();
+//     console.log(localStorage.getItem(STORAGE_KEY));
+//     localStorage.removeItem(STORAGE_KEY);
+//     }
+
+// function populateForm(){
+//     const savedStorage = localStorage.getItem(STORAGE_KEY)
+
+//     if(savedStorage){
+//         const savedForm = JSON.parse(savedStorage);
+//         console.log(savedForm)
+//         Object.entries(savedForm).forEach(([name, value]) => {
+//             form.elements[name].value = value; })
+
+//     }
+// }
+
+import trottle from 'lodash.throttle';
+
+const form = document.querySelector('form');
 const STORAGE_KEY = 'feedback-form-state';
+//const formData = {};
+populateForm();
 
-onFillingForm();
-
-formEl.addEventListener('submit', onFormSubmit);
-formEl.addEventListener(
+form.addEventListener('submit', onSubmitedForm);
+form.addEventListener(
   'input',
-  throttle(e => {
+  trottle(e => {
     let completedForm = localStorage.getItem(STORAGE_KEY);
     completedForm = completedForm ? JSON.parse(completedForm) : {};
     completedForm[e.target.name] = e.target.value;
@@ -18,21 +54,21 @@ formEl.addEventListener(
   500
 );
 
-function onFormSubmit(e) {
+function onSubmitedForm(e) {
   e.preventDefault();
   e.currentTarget.reset();
   console.log(localStorage.getItem(STORAGE_KEY));
   localStorage.removeItem(STORAGE_KEY);
 }
 
-function onFillingForm() {
+function populateForm() {
   const savedStorage = localStorage.getItem(STORAGE_KEY);
 
   if (savedStorage) {
     const savedForm = JSON.parse(savedStorage);
     console.log(savedForm);
     Object.entries(savedForm).forEach(([name, value]) => {
-      formEl.elements[name].value = value;
+      form.elements[name].value = value;
     });
   }
 }
